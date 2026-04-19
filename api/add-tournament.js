23195123b935da284f8a8d7b6b620501a778a772
semwalpriyanshu2007-hub <semwalpriyanshu2007@gmail.com>
2026-04-19 09:53:34 +0530

    Update add-tournament.js

--- api/add-tournament.js
+++ api/add-tournament.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    const { name, price } = req.body;
+    const { name, price } = req.body || {};
 
     return res.status(200).json({
       success: true,
