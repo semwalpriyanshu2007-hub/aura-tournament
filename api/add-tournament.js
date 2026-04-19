1ee2b41c6b81a17d1afae68e0a414d54ab69fa62
semwalpriyanshu2007-hub <semwalpriyanshu2007@gmail.com>
2026-04-19 09:47:08 +0530

    Create add-tournament.js

---
+++ api/add-tournament.js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, price } = req.body;
+
+    return res.status(200).json({
+      success: true,
+      message: 'Tournament added',
+      data: { name, price }
+    });
+  }
+
+  res.status(405).json({ message: 'Method not allowed' });
+}
